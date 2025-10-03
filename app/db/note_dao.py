c3723d6b0f3411bb1282d9dbb708917e98a46570
annacampagna <anna.campagna13@icloud.com>
2025-10-03 18:00:11 +0200

    fix(dao): fixed Note insertion method to also accept the Topic associated with the Note and edited the updating method to only give the possibility to edit the content of the Note

--- app/db/note_dao.py
+++ app/db/note_dao.py
@@ -7,10 +7,12 @@ class NoteDAO:
 
     def insert_note(self, note: Note):
         self.db.cursor.execute(
-            """ INSERT INTO notes (title, content, created_at)
-                VALUES (?, ?, ?) """,
-        (note.title, note.content, note.created_at.isoformat()))
+            """ INSERT INTO notes (title, topic_id, content, created_at)
+                VALUES (?, ?, ?, ?) """,
+        (note.title, note.topic.id if note.topic else None, note.content, note.created_at.isoformat()))
         self.db.commit()
+        # Returns the ID of the inserted note
+        return self.db.cursor.lastrowid
 
     def get_all_notes(self):
         self.db.cursor.execute("SELECT * FROM notes ORDER BY created_at DESC")
@@ -24,10 +26,9 @@ class NoteDAO:
         self.db.cursor.execute("DELETE FROM notes WHERE id = ?", (note_id,))
         self.db.commit()
 
-    def update_note(self, note_id: int, new_title: str, new_content: str):
+    def update_note(self, note_id, new_content):
         self.db.cursor.execute(
-            """ UPDATE notes
-                SET title = ?, content = ?
-                WHERE id = ? """, 
-        (new_title, new_content, note_id))
-        self.db.commit()
+            "UPDATE notes SET content = ? WHERE id = ?",
+            (new_content, note_id)
+        )
+        self.db.connection.commit()
