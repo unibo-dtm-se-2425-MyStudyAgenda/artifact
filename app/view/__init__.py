29e31683200ae7f96138231a971c26512ef865d1
annacampagna <anna.campagna13@icloud.com>
2025-10-04 16:43:35 +0200

    chore(view): add __init__.py to expose view submodules as a package

---
+++ app/view/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["notes_screen", "planner_screen", "task_screen", "pomodoro_screen"]
+
+from . import notes_screen
+from . import planner_screen
+from . import task_screen
+from . import pomodoro_screen
