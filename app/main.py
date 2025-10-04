4d402c5935626ccf0aad3d35982882467a9a0674
annacampagna <anna.campagna13@icloud.com>
2025-10-04 18:20:26 +0200

    refactor(main): centralize controller initialization in main to share them across views

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ from app.view.task_screen import TaskScreen
 from app.view.planner_screen import PlannerScreen
 from app.view.notes_screen import NotesScreen
 from app.view.pomodoro_screen import PomodoroScreen
+from app.controller.topic_controller import TopicController
+from app.controller.task_controller import TaskController
+from app.controller.note_controller import NoteController
 
 Builder.load_file("view/nav_bar.kv")
 Builder.load_file("view/task_screen.kv")
@@ -14,6 +17,9 @@ Builder.load_file("view/pomodoro_screen.kv")
 
 class MyStudyAgenda(App):
     def build(self):
+        self.topic_controller = TopicController()
+        self.task_controller = TaskController()
+        self.note_controller = NoteController()
         self.sm = ScreenManager()
         self.sm.add_widget(TaskScreen(name="tasks"))
         self.sm.add_widget(PlannerScreen(name="planner"))
