f3b56f70d2d0cbc9f3c1ccb1ff20637c3cb143bc
annacampagna <anna.campagna13@icloud.com>
2025-10-04 16:47:09 +0200

    feat(main): integrate screen manager with navigation bar and GUI screens

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from app.view.task_screen import TaskScreen
+from app.view.planner_screen import PlannerScreen
+from app.view.notes_screen import NotesScreen
+from app.view.pomodoro_screen import PomodoroScreen
+
+Builder.load_file("view/nav_bar.kv")
+Builder.load_file("view/task_screen.kv")
+Builder.load_file("view/planner_screen.kv")
+Builder.load_file("view/notes_screen.kv")
+Builder.load_file("view/pomodoro_screen.kv")
 
 class MyStudyAgenda(App):
     def build(self):
         self.sm = ScreenManager()
+        self.sm.add_widget(TaskScreen(name="tasks"))
+        self.sm.add_widget(PlannerScreen(name="planner"))
+        self.sm.add_widget(NotesScreen(name="notes"))
+        self.sm.add_widget(PomodoroScreen(name="pomodoro"))
         return self.sm
 
 if __name__ == "__main__":
